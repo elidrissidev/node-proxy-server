8ab3584aa7886cc2bd0e87b02fd963ad536d4c0e
Mohamed ELIDRISSI <67818913+elidrissidev@users.noreply.github.com>
2022-05-19 23:12:58 +0100

    Refactor request options creation into a new method

--- src/lib/http-proxy/index.js
+++ src/lib/http-proxy/index.js
@@ -2,36 +2,44 @@ import http from 'node:http'
 
 export class HttpProxy {
   /**
+   * Build target request options from client request
+   *
+   * @param {http.IncomingMessage} req
+   * @returns {http.RequestOptions}
+   */
+  #getRequestOptions(req) {
+    const reqUrl = new URL(req.url, `http://${req.headers.host}`)
+
+    return {
+      hostname: reqUrl.hostname,
+      port: reqUrl.port,
+      path: reqUrl.pathname,
+      method: req.method,
+      headers: {
+        ...req.headers,
+      },
+    }
+  }
+
+  /**
    * Handle proxying client request to target
    *
    * @param {http.IncomingMessage} req
    * @param {http.ServerResponse} res
    */
   handleRequest(req, res) {
-    const reqUrl = new URL(req.url, `http://${req.headers.host}`)
+    const options = this.#getRequestOptions(req)
+    const proxyReq = http.request(options, (proxyRes) => {
+      res.statusCode = proxyRes.statusCode
+      res.statusMessage = proxyRes.statusMessage
 
-    const proxyReq = http.request(
-      {
-        hostname: reqUrl.hostname,
-        port: reqUrl.port,
-        path: reqUrl.pathname,
-        method: req.method,
-        headers: {
-          ...req.headers,
-        },
-      },
-      (proxyRes) => {
-        res.statusCode = proxyRes.statusCode
-        res.statusMessage = proxyRes.statusMessage
-
-        // Copy all target response headers to client response
-        Object.entries(proxyRes.headers).forEach(([key, value]) => {
-          res.setHeader(key, value)
-        })
-        // Stream response from target back to client
-        proxyRes.pipe(res)
-      }
-    )
+      // Copy all target response headers to client response
+      Object.entries(proxyRes.headers).forEach(([key, value]) => {
+        res.setHeader(key, value)
+      })
+      // Stream response from target back to client
+      proxyRes.pipe(res)
+    })
 
     // Stream request from client to target
     req.pipe(proxyReq)
