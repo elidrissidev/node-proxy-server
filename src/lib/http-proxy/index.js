ac9ed67c89dfd625308aca7d192a485f6716a66a
Mohamed ELIDRISSI <67818913+elidrissidev@users.noreply.github.com>
2022-05-20 22:22:40 +0100

    Override Host header in proxy request with target host

--- src/lib/http-proxy/index.js
+++ src/lib/http-proxy/index.js
@@ -67,10 +67,11 @@ export class HttpProxy {
     return {
       hostname: target.hostname,
       port: target.port,
-      path: target.pathname,
+      path: requestUrl.pathname,
       method: req.method,
       headers: {
         ...req.headers,
+        host: target.hostname,
         // Let the target server know who the client is by sending the standard forwarding headers.
         // Ref: https://developer.mozilla.org/en-US/docs/Web/HTTP/Proxy_servers_and_tunneling#forwarding_client_information_through_proxies
         // TODO: Chain the headers with the ones from the request if they exist (i.e. if the request was from another proxy).
