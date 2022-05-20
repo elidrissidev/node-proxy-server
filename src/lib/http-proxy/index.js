723bf349ffa4fe4b83cb914cd37544f783407634
Mohamed ELIDRISSI <67818913+elidrissidev@users.noreply.github.com>
2022-05-20 10:30:30 +0100

    Send forwarding headers to target server

--- src/lib/http-proxy/index.js
+++ src/lib/http-proxy/index.js
@@ -17,6 +17,13 @@ export class HttpProxy {
       method: req.method,
       headers: {
         ...req.headers,
+        // Let the target server know who the client is by sending the standard forwarding headers.
+        // Ref: https://developer.mozilla.org/en-US/docs/Web/HTTP/Proxy_servers_and_tunneling#forwarding_client_information_through_proxies
+        // TODO: Chain the headers with the ones from the request if they exist (i.e. if the request was from another proxy).
+        'x-forwarded-for': req.socket.remoteAddress,
+        'x-forwarded-host': reqUrl.hostname,
+        'x-forwarded-port': reqUrl.port,
+        'x-forwarded-proto': reqUrl.port.replace(':', ''),
       },
     }
   }
